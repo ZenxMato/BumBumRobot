c3395c352afd1b146a2f4fb8f2f40f128adf70a2
ZenxMato <97756244+ZenxMato@users.noreply.github.com>
2022-03-01 13:58:09 +0700

    Update ping.py

--- SiestaRobot/modules/ping.py
+++ SiestaRobot/modules/ping.py
@@ -79,9 +79,9 @@ def ping(update: Update, context: CallbackContext):
     uptime = get_readable_time((time.time() - StartTime))
 
     message.edit_text(
-        "<b>PONG</b> ✨\n"
-        "<b>Time Taken:</b> <code>{}</code>\n"
-        "<b>Service Uptime:</b> <code>{}</code>".format(telegram_ping, uptime),
+        "<b>PONG</b> 💫\n"
+        "<b>Time Already:</b> <code>{}</code>\n"
+        "<b>Uptime Bot:</b> <code>{}</code>".format(telegram_ping, uptime),
         parse_mode=ParseMode.HTML,
     )
 
