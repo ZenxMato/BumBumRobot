24f9d21f1978ec21d2c07d315eae014b481aab42
ZenxMato <97756244+ZenxMato@users.noreply.github.com>
2022-03-01 13:08:24 +0700

    Update misc.py

--- SiestaRobot/modules/misc.py
+++ SiestaRobot/modules/misc.py
@@ -66,7 +66,7 @@ def markdown_help_sender(update: Update):
 def markdown_help(update: Update, context: CallbackContext):
     if update.effective_chat.type != "private":
         update.effective_message.reply_text(
-            "Contact me in pm",
+            "🐻 Unch Gladdy Contact Me In PM",
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
